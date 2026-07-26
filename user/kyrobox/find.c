60533ca359e3677a1143ada904a4a2a40d2c7e7f
z3nnix <zennix@tuta.io>
2026-07-26 23:55:34 +0300

    feat(userspace): add more flags in ls, add -type in find, update prompt in kxsh

--- user/kyrobox/find.c
+++ user/kyrobox/find.c
@@ -1,7 +1,9 @@
 #include "common.h"
 #include <fnmatch.h>
 
-static const char *name_pat;
+static const char *name_pat = NULL;
+static const char *type_filter = NULL;
+static const char *exec_cmd = NULL;
 
 static int walk(const char *path) {
     struct stat st;
@@ -9,7 +11,46 @@ static int walk(const char *path) {
         kx_warn(path);
         return 1;
     }
-    if (!name_pat || fnmatch(name_pat, kx_base(path), 0) == 0) puts(path);
+
+    int match = 1;
+    if (name_pat && fnmatch(name_pat, kx_base(path), 0) != 0) match = 0;
+    if (type_filter) {
+        char ft = 'f';
+        if (S_ISDIR(st.st_mode)) ft = 'd';
+        else if (S_ISLNK(st.st_mode)) ft = 'l';
+        else if (S_ISBLK(st.st_mode)) ft = 'b';
+        else if (S_ISCHR(st.st_mode)) ft = 'c';
+        else if (S_ISFIFO(st.st_mode)) ft = 'p';
+        else if (S_ISSOCK(st.st_mode)) ft = 's';
+        if (ft != type_filter[0]) match = 0;
+    }
+
+    if (match) {
+        if (exec_cmd) {
+            char cmd[PATH_MAX + 1024];
+            const char *p = exec_cmd;
+            char *dst = cmd;
+            char *end = cmd + sizeof(cmd) - 1;
+            while (*p && dst < end) {
+                if (*p == '{' && p[1] == '}') {
+                    size_t plen = strlen(path);
+                    if (dst + plen < end) {
+                        memcpy(dst, path, plen);
+                        dst += plen;
+                    }
+                    p += 2;
+                } else {
+                    *dst++ = *p++;
+                }
+            }
+            *dst = '\0';
+            int rc = system(cmd);
+            if (rc != 0) return rc;
+        } else {
+            puts(path);
+        }
+    }
+
     if (!S_ISDIR(st.st_mode)) return 0;
     DIR *d = opendir(path);
     if (!d) {
@@ -33,11 +74,17 @@ int main(int argc, char **argv) {
     const char *root = ".";
     int i = 1;
     if (i < argc && argv[i][0] != '-') root = argv[i++];
-    if (i < argc) {
-        if (i + 1 < argc && strcmp(argv[i], "-name") == 0)
-            name_pat = argv[i + 1];
-        else
-            kx_die("usage: find [PATH] [-name PATTERN]");
+    while (i < argc) {
+        if (strcmp(argv[i], "-name") == 0 && i + 1 < argc) {
+            name_pat = argv[++i];
+        } else if (strcmp(argv[i], "-type") == 0 && i + 1 < argc) {
+            type_filter = argv[++i];
+        } else if (strcmp(argv[i], "-exec") == 0 && i + 1 < argc) {
+            exec_cmd = argv[++i];
+        } else {
+            kx_die("usage: find [PATH] [-name PATTERN] [-type f|d|l] [-exec CMD {}]");
+        }
+        i++;
     }
     return walk(root);
 }
